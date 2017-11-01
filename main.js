536609cd8231fa82d2496b0e3afcbe8ef541a95a
Mohammed Salman <msal4@outlook.com>
2017-11-01 15:41:20 +0300

    R

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game("100%", "100%", Phaser.AUTO);
+let game = new Phaser.Game("100%", "100%", Phaser.AUTO);
 game.state.add("BootState", BootState);
 game.state.add("PreloadState", PreloadState);
 game.state.add("HomeState", HomeState);
